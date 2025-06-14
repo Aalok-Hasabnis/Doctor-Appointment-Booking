a8edbd08f90f5428392576d24dc03ff102491fbb
Aalok Hasabnis <aalokhasabnis13@gmail.com>
2025-06-14 15:48:05 +0530

    ..

--- app/layout.js
+++ app/layout.js
@@ -28,7 +28,7 @@ export default function RootLayout({ children }) {
 
           <footer className="bg-muted/50 py-12">
             <div className="container mx-auto px-4 text-center text-gray-200">
-              <p>Made for Booking Appointments</p>
+              <p>Made for seeking any professinal healthCare Advice</p>
             </div>
           </footer>
         </ThemeProvider>
